232ca8fc48edfd48482ac6d6344e231b047dee62
Blake Bourque <techplex.engineer@gmail.com>
2024-01-24 16:13:34 -0500

    Add facilities calendar

--- src/slack/blocks/app_home_blocks.ts
+++ src/slack/blocks/app_home_blocks.ts
@@ -1,226 +1,221 @@
 const blocks = [
-        {
-            "type": "header",
-            "text": {
-                "type": "plain_text",
-                "text": "Team Quick Links",
-                "emoji": true
-            }
+    {
+        "type": "header",
+        "text": {
+            "type": "plain_text",
+            "text": "Team Quick Links",
+            "emoji": true
+        }
+    },
+    {
+        "type": "divider"
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Bionics Docs*\n\n Bionics docs is team team's core documentation site with information about our tools and processes."
         },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*Bionics Docs*\n\n Bionics docs is team team's core documentation site with information about our tools and processes."
-            },
-            // "accessory": {
-            //     "type": "image",
-            //     "image_url": "https://docs.team4909.org/images/bionics-docs-logo.png",
-            //     "alt_text": "Bionics Docs Logo"
-            // }
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Bionics Docs ➚",
-                        "emoji": true
-                    },
-                    "url": "https://docs.team4909.org/home/",
-                    // "style": "primary"
-                }
-            ]
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*Trello*\n\n Used to organize ourselves and keep track of work.",
-                "verbatim": true
-            },
-            // "accessory": {
-            //     "type": "image",
-            //     "image_url": "https://docs.team4909.org/images/trello-logo.png",
-            //     "alt_text": "plane"
-            // }
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Trello Invite ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/invite/team4909bionics/ATTIfa132292b74632c5cc038925957c228594F5FB12",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Software ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/b/KkO4oId1/software",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Operations ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/b/kIldmPwM/operations",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Robot ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/b/e2LD6Dta/robot",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Climber ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/b/QOTvMOUu/climber",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Shooter ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/b/HrgYcUQR/shooter",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Arm ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/b/ZoraMaCR/arm",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Intake ➚",
-                        "emoji": true
-                    },
-                    "url": "https://trello.com/b/WBSaLo8l/intake",
-                    // "style": "primary"
-                }
-
-            ]
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*Order Sheet*\n\n We use a lot of parts and tools during the build season. We use the order sheet spreadsheet to track items to order and our order history."
-            },
-            // "accessory": {
-            //     "type": "image",
-            //     "image_url": "https://docs.team4909.org/images/google-sheets.png",
-            //     "alt_text": "Order Sheet"
-            // }
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Order Sheet ➚",
-                        "emoji": true
-                    },
-                    "url": "https://docs.google.com/spreadsheets/d/1BGMXljo7L7N766PpeVszUuOC9rf_mTWG5kIJSzcwGbo/edit#gid=1432125071",
-                    // "style": "primary"
-                }
-            ]
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*Canva*\n\n Canva is a flexible design tool for print and digital media."
-            },
-            // "accessory": {
-            //     "type": "image",
-            //     "image_url": "https://docs.team4909.org/images/canva.png",
-            //     "alt_text": "Canva Logo"
-            // }
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Canva Team Join ➚",
-                        "emoji": true
-                    },
-                    "url": "https://www.canva.com/brand/join?token=rMSOBEAxDYw0KxI8wLByjQ&brandingVariant=edu&referrer=team-invite",
-                    // "style": "primary"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Shared Canva Folder ➚",
-                        "emoji": true
-                    },
-                    "url": "https://www.canva.com/folder/FAFOjWxLvTo",
-                    // "style": "primary"
-                }
-            ]
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Meal Train ➚",
-                        "emoji": true
-                    },
-                    "url": "https://www.mealtrain.com/trains/3m55w",
-                    // "style": "primary"
-                }
-            ]
+        // "accessory": {
+        //     "type": "image",
+        //     "image_url": "https://docs.team4909.org/images/bionics-docs-logo.png",
+        //     "alt_text": "Bionics Docs Logo"
+        // }
     },
     {
         "type": "actions",
         "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Bionics Docs ➚",
+                    "emoji": true
+                },
+                "url": "https://docs.team4909.org/home/",
+                // "style": "primary"
+            }
+        ]
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Trello*\n\n Used to organize ourselves and keep track of work.",
+            "verbatim": true
+        },
+        // "accessory": {
+        //     "type": "image",
+        //     "image_url": "https://docs.team4909.org/images/trello-logo.png",
+        //     "alt_text": "plane"
+        // }
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Trello Invite ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/invite/team4909bionics/ATTIfa132292b74632c5cc038925957c228594F5FB12",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Software ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/b/KkO4oId1/software",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Operations ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/b/kIldmPwM/operations",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Robot ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/b/e2LD6Dta/robot",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Climber ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/b/QOTvMOUu/climber",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Shooter ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/b/HrgYcUQR/shooter",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Arm ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/b/ZoraMaCR/arm",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Intake ➚",
+                    "emoji": true
+                },
+                "url": "https://trello.com/b/WBSaLo8l/intake",
+                // "style": "primary"
+            }
+
+        ]
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Order Sheet*\n\n We use a lot of parts and tools during the build season. We use the order sheet spreadsheet to track items to order and our order history."
+        },
+        // "accessory": {
+        //     "type": "image",
+        //     "image_url": "https://docs.team4909.org/images/google-sheets.png",
+        //     "alt_text": "Order Sheet"
+        // }
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Order Sheet ➚",
+                    "emoji": true
+                },
+                "url": "https://docs.google.com/spreadsheets/d/1BGMXljo7L7N766PpeVszUuOC9rf_mTWG5kIJSzcwGbo/edit#gid=1432125071",
+                // "style": "primary"
+            }
+        ]
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Canva*\n\n Canva is a flexible design tool for print and digital media."
+        },
+        // "accessory": {
+        //     "type": "image",
+        //     "image_url": "https://docs.team4909.org/images/canva.png",
+        //     "alt_text": "Canva Logo"
+        // }
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Canva Team Join ➚",
+                    "emoji": true
+                },
+                "url": "https://www.canva.com/brand/join?token=rMSOBEAxDYw0KxI8wLByjQ&brandingVariant=edu&referrer=team-invite",
+                // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Shared Canva Folder ➚",
+                    "emoji": true
+                },
+                "url": "https://www.canva.com/folder/FAFOjWxLvTo",
+                // "style": "primary"
+            }
+        ]
+    },
+    {
+        "type": "divider"
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Meal Train ➚",
+                    "emoji": true
+                },
+                "url": "https://www.mealtrain.com/trains/3m55w",
+                // "style": "primary"
+            },        
             {
                 "type": "button",
                 "text": {
@@ -230,12 +225,23 @@ const blocks = [
                 },
                 "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vR5cai8OfslQuqXyvGn8zBRWkoAAo-5-h5nNb8hvzJVw9Y0yv3dsaGFzxbn0_jhc3VrRLGvPracLan6/pubhtml?gid=1674155148&single=true",
                 // "style": "primary"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Facilities Calendar ➚",
+                    "emoji": true
+                },
+                "url": "https://fs-billerica.rschooltoday.com/calendar/new/index/defaultView//viewtype/1",
+                // "style": "primary"
             }
+            
         ]
     }
 
-        
-    ];
+
+];
 export default {
     type: 'home',
     title: {
