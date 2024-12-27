fd582f572e6028cbe90ed51e11704e4c8f93a7a2
Blake Bourque <techplex.engineer@gmail.com>
2024-12-27 14:46:19 -0500

    Easypost

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,3 +1,4 @@
-export default async (request: Request) => {
+export default async (request: Request, env Bindings) => {
+    console.log("env.EASYPOST_API_KEY", env.EASYPOST_API_KEY);
     return new Response("Hello, I'm Bionic Bob");
 };
